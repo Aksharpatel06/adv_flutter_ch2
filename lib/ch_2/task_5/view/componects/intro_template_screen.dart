1dfacb76eab61170143f5d1009abce2d18f561a3
Aksharpatel06 <aksharpatel030306@gmail.com>
2024-05-13 20:08:09 +0530

    13-05-2024

--- lib/ch_2/task_5/view/componects/intro_template_screen.dart
+++ lib/ch_2/task_5/view/componects/intro_template_screen.dart
@@ -7,52 +7,31 @@ class IntroTemplateScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color(0xffFFF2BD),
-      child: Center(
-        child: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('asset/img/img_1.png'), fit: BoxFit.cover)),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 25,
-              ),
-              Row(
-                children: [
-                  Spacer(),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Skip',
-                      style: Theme.of(context)
-                          .textTheme
-                          .titleMedium!
-                          .copyWith(fontWeight: FontWeight.w400, fontSize: 18),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 15,
-                  )
-                ],
-              ),
-              Spacer(),
-              introTextandButtom(
-                  context: context,
-                  height1: 10,
-                  width1: 25,
-                  height2: 10,
-                  width2: 10,
-                  height3: 10,
-                  width3: 10,
-                  title: "Select Template",
-                  subtitle:
-                      "Welcome to Our Platform! Where innovation meets creativity . Connect, collaborate, and create . Your journey begins here . Let's build something amazing together.",
-                  buttomntext: "Next",
-                  color: Colors.orange)
-            ],
-          ),
+    return Center(
+      child: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('asset/img/img_1.png'), fit: BoxFit.cover)),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 25,
+            ),
+            Spacer(),
+            introTextandButtom(
+                context: context,
+                height1: 10,
+                width1: 25,
+                height2: 10,
+                width2: 10,
+                height3: 10,
+                width3: 10,
+                title: "Select Template",
+                subtitle:
+                    "Welcome to Our Platform! Where innovation meets creativity . Connect, collaborate, and create . Your journey begins here . Let's build something amazing together.",
+                buttomntext: "Next",
+                color: Colors.orange)
+          ],
         ),
       ),
     );
--- lib/ch_2/task_5/view/componects/intro_working_screen.dart
+++ lib/ch_2/task_5/view/componects/intro_working_screen.dart
@@ -8,47 +8,28 @@ class IntroWorkingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color(0xffFFF2BD),
-      child: Center(
-        child: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('asset/img/img_3.png'), fit: BoxFit.cover)),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                children: [
-                  CupertinoButton(
-                    child: Icon(
-                      CupertinoIcons.back,
-                      color: Colors.black,
-                    ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ],
-              ),
-              Spacer(),
-              introTextandButtom(
-                  context: context,
-                  height1: 10,
-                  width1: 10,
-                  height2: 10,
-                  width2: 10,
-                  height3: 10,
-                  width3: 25,
-                  title: "Start Working",
-                  subtitle:
-                      "Your journey is just beginning. Share your experiences, Collaborate with peers, Achieve your goals, And enjoy the adventure.",
-                  buttomntext: "Get Started",
-                  color: Colors.redAccent)
-            ],
-          ),
+    return Center(
+      child: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('asset/img/img_3.png'), fit: BoxFit.cover)),
+        child: Column(
+          children: [
+            Spacer(),
+            introTextandButtom(
+                context: context,
+                height1: 10,
+                width1: 10,
+                height2: 10,
+                width2: 10,
+                height3: 10,
+                width3: 25,
+                title: "Start Working",
+                subtitle:
+                    "Your journey is just beginning. Share your experiences, Collaborate with peers, Achieve your goals, And enjoy the adventure.",
+                buttomntext: "Get Started",
+                color: Colors.redAccent)
+          ],
         ),
       ),
     );
